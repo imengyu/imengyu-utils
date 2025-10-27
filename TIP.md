0c3cffa9f2780f1334fe9bb9020242019e32483d
快乐的梦鱼 <32213395+imengyu@users.noreply.github.com>
2025-10-27 15:03:26 +0800

    修改为vite编译

--- TIP.md
+++
@@ -1,2 +0,0 @@
-npm config set registry https://registry.npmjs.com/   
-npm config set registry https://registry.npmmirror.com
---
+++ vite.config.ts
@@ -0,0 +1,35 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import dts from 'vite-plugin-dts';
+import path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    dts()
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      formats: ['es', 'umd'],
+      name: 'imengyu-utils',
+      fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['vue', '@imengyu/js-request-transform'],
+      output: {
+        globals: {
+          vue: 'Vue',
+          '@imengyu/js-request-transform': 'JsRequestTransform'
+        }
+      }
+    },
+    sourcemap: true
+  }
+});
