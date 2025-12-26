b5f03a38249633e3d48505b5ae57527d42921c5e
快乐的梦鱼 <32213395+imengyu@users.noreply.github.com>
2025-12-26 14:14:11 +0800

    优化设置工具类

--- src/SettingsUtils.ts
+++ src/SettingsUtils.ts
@@ -3,10 +3,32 @@ import StringUtils from "./StringUtils";
 
 let prefix = 'Settings_';
 
+export interface SettingsUtilsProvider {
+  getSettings(key : string) : any;
+  setSettings(key : string, value : any) : void;
+  deleteSettings(key : string) : void;
+}
+
+export const LocalStorageSettingsUtilsProvider : SettingsUtilsProvider = {
+  getSettings(key : string) : any {
+    return localStorage.getItem(key);
+  },
+  setSettings(key : string, value : any) : void {
+    localStorage.setItem(key, value);
+  },
+  deleteSettings(key : string) : void {
+    localStorage.removeItem(key);
+  },
+}
+
 /**
  * 设置工具类. 基于LocalStorage
  */
 const SettingsUtils = {
+  provider: LocalStorageSettingsUtilsProvider,
+  setProvider(provider: SettingsUtilsProvider) {
+    this.provider = provider;
+  },
   /**
    * 设定设置条目在 localStorage 中存储键值前缀
    * @param _prefix 前缀
@@ -21,7 +43,7 @@ const SettingsUtils = {
    * @param copyUndefinedFromDefault 当遇到空对象时是否从默认设置拷贝
    */
   getSettings<T extends Record<string,any>|bigint|number|boolean|Array<unknown>|string>(key : string, defaultValue : T, copyUndefinedFromDefault = true) : T {
-    const set = localStorage.getItem(`${prefix}${key}`);
+    const set = this.provider.getSettings(`${prefix}${key}`);
     if(!set || StringUtils.isNullOrEmpty(set))
       return defaultValue;
     let ret = JSON.parse(set) as T;
@@ -30,12 +52,19 @@ const SettingsUtils = {
     return ret;
   },
   /**
+   * 删除设置
+   * @param key 设置的键值
+   */
+  removeSettings(key : string) : void {
+    this.provider.deleteSettings(`${prefix}${key}`);
+  },
+  /**
    * 设置设置
    * @param key 设置的键值
    * @param value 设置的新值
    */
   setSettings(key : string, value : Record<string,any>|number|boolean|bigint|Array<unknown>|string) : void {
-    localStorage.setItem(`${prefix}${key}`, JSON.stringify(value));
+    this.provider.setSettings(`${prefix}${key}`, JSON.stringify(value));
   },
 }
 
